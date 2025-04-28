375dfc223a6e193675e3f3455247efac8e97385b
kieranichiban <150093566+kieranichiban@users.noreply.github.com>
2025-04-28 15:48:01 -0600

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,14 @@
+// This is an empty JavaScript file.
+// You can start writing your JavaScript code here.
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
